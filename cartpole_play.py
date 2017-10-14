86daab45690bc5b578dfa81cad612a99698ed78b
pyemma <yangpei@Yangs-MacBook-Pro.local>
2017-10-14 09:42:00 -0700

    update

---
+++ cartpole_play.py
@@ -0,0 +1,64 @@
+import gym
+from dqn import DQN
+
+
+class CartPolePlay(object):
+
+    def __init__(
+        self,
+        hidden_dims,
+        step_to_copy_graph=300,
+        step_each_epsiode=500,
+    ):
+        self.step_to_copy_graph = step_to_copy_graph
+        self.step_each_epsiode = step_each_epsiode
+
+        self.dqn = DQN(4, 2, hidden_dims)
+        self.env = gym.make('CartPole-v0')
+
+    def train(self, num_train=5000):
+        running_score = 0.0
+        num_epsiode = 0
+        num_step = 0
+
+        for _ in range(num_train):
+            state = self.env.reset()
+
+            for t in range(self.step_each_epsiode):
+                num_step += 1
+                action = self.dqn.action(state)
+                next_state, reward, done, _ = self.env.step(action)
+                reward = -100 if done else 0.1
+                self.dqn.remember(state, action, reward, done, next_state)
+                state = next_state
+
+                self.dqn.learn()
+
+                if num_step % self.step_to_copy_graph == 0:
+                    self.dqn.copy_graph()
+
+                if done:
+                    running_score += t
+                    break
+
+            num_epsiode += 1
+            self.dqn.decrease_epsilon()
+            if num_epsiode % 100 == 0:
+                print("Current running score is: %.2f" % (running_score / 100))
+                running_score = 0.0
+
+    def play(self, num_epsiode):
+        total_score = 0.0
+        for _ in range(num_epsiode):
+            state = self.env.reset()
+            while True:
+                action = self.dqn.play(state)
+                next_state, reward, done, _ = self.env.step(action)
+                if done:
+                    break
+                total_score += 1
+                state = next_state
+        return total_score / num_epsiode
+
+    def store(self):
+        self.dqn.save('model/dqn/cartpole-v0.ckpt')
--- dqn_play.py
+++ dqn_play.py
@@ -1,7 +1,6 @@
-import gym
 from dqn import DQN
+from cartpole_play import CartPolePlay
 
-env = gym.make('CartPole-v0')
 
 MAX_EPSIODE = 5000
 STEPS_TO_COPY_GRAPH = 300
@@ -11,48 +10,16 @@ NUM_TRAIN = 10
 best_eval_score = 0
 
 for _ in range(2):
-    dqn = DQN(4, 2, [10, 20, 10])
-    running_score = 0.0
-    num_epsiode = 0
-    num_steps = 0
+    cartpole = CartPolePlay(
+        hidden_dims=[10, 20, 10],
+        step_to_copy_graph=STEPS_TO_COPY_GRAPH,
+        step_each_epsiode=STEPS_EACH_EPSIODE
+    )
 
-    for _ in range(1000):
-        state = env.reset()
+    cartpole.train(num_train=1000)
+    score = cartpole.play(10)
 
-        for t in range(500):
-            num_steps += 1
-            action = dqn.action(state)
-            next_state, reward, done, _ = env.step(action)
-            reward = -100 if done else 0.1
-            dqn.remember(state, action, reward, done, next_state)
-            state = next_state
-
-            dqn.learn()
-
-            if num_steps % 300 == 0:
-                dqn.copy_graph()
-
-            if done:
-                running_score += t
-                break;
-
-        num_epsiode += 1
-        dqn.decrease_epsilon()
-        if num_epsiode % 100 == 0:
-            dqn.decrease_epsilon()
-            print("Current running score is: %.2f" %(running_score / 100))
-            running_score = 0.0
-
-    total_score = 0
-    for _ in range(10):
-        state = env.reset()
-        while True:
-            action = dqn.play(state)
-            next_state, reward, done, _ = env.step(action)
-            if done:
-                break
-            total_score += 1
-            state = next_state
-    print("Evalution score: %d" % (total_score))
-    if total_score > best_eval_score:
-        dqn.save('model/dqn/cartpole-v0.ckpt')
+    if score > best_eval_score:
+        print("Find new best score %.2f" % score)
+        best_eval_score = score
+        cartpole.store()
